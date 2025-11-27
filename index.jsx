47d4879ef91ffa195887f6fb4470bb43be4cc332
BSummers32 <brendan.summers32@gmail.com>
2025-11-26 22:40:03 -0600

    Delete index.jsx

--- index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import BattleSimulator from './App'; // Looks for App.jsx in the same folder
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BattleSimulator />
-  </React.StrictMode>
-);
